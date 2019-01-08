448142657769983672971cb7e994e2b8e6e47fd2
Vincent Villalta <vincent@stopbreathethink.com>
2019-01-07 21:15:29 -0600

    Initial Commit

---
+++ BitConvert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitConvert
+//
+//  Created by Vincent Villalta on 1/7/19.
+//  Copyright © 2019 Vincent Villalta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
